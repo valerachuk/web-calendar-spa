e770ed696636ed0e3618a14074c8270e8ffb95f7
valerachuk <valerachuk@gmail.com>
2020-11-05 00:21:40 +0200

    #14 sending emails implemented

---
+++ web-calendar-api/WebCalendar.Business/Common/EmailNotificationsOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace WebCalendar.Business.Common
+{
+  public class EmailNotificationsOptions
+  {
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string EmailSubject { get; set; }
+    public string SenderDisplayName { get; set; }
+    public string HTMLEmailTemplatePath { get; set; }
+  }
+
+}
---
+++ web-calendar-api/WebCalendar.Business/Domains/NotificationSenderDomain.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Options;
+using WebCalendar.Business.Common;
+using WebCalendar.Business.Domains.Interfaces;
+using WebCalendar.Data.Repositories.Interfaces;
+
+namespace WebCalendar.Business.Domains
+{
+  public class NotificationSenderDomain : INotificationSenderDomain
+  {
+    private readonly IOptions<EmailNotificationsOptions> _emailSenderOptions;
+    private readonly IUserRepository _userRepository;
+
+    public NotificationSenderDomain(
+      IOptions<EmailNotificationsOptions> emailSenderOptions,
+      IUserRepository userRepository
+      )
+    {
+      _userRepository = userRepository;
+      _emailSenderOptions = emailSenderOptions;
+    }
+
+    private void SendEmail(string recipientEmail, string message)
+    {
+      var emailSenderOptions = _emailSenderOptions.Value;
+
+      using var client = new SmtpClient(emailSenderOptions.Host, emailSenderOptions.Port)
+      {
+        Credentials = new NetworkCredential(emailSenderOptions.Login, emailSenderOptions.Password),
+        EnableSsl = true
+      };
+
+      var emailTemplate = File.ReadAllText(emailSenderOptions.HTMLEmailTemplatePath);
+      using var mailMessage = new MailMessage(new MailAddress(emailSenderOptions.Login, emailSenderOptions.SenderDisplayName), new MailAddress(recipientEmail))
+      {
+        Body = emailTemplate.Replace("__CONTENT__", message),
+        Subject = emailSenderOptions.EmailSubject,
+        IsBodyHtml = true
+      };
+
+      client.Send(mailMessage);
+    }
+
+    public void NotifyEventCreated(string eventName, int userId)
+    {
+      var user = _userRepository.GetUser(userId);
+      if (!user.ReceiveEmailNotifications) return;
+
+      var notificationMessage = $@"
+        Hello <i>{ user.FirstName },</i>
+        <br>Event <b>{ eventName }</b> has been successfully created.
+      ";
+
+      SendEmail(user.Email, notificationMessage);
+    }
+  }
+}
